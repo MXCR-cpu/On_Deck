d7536c8b80e4f3c33cde814ab4cda4b13b3d4059
Maximus Cisneros <mxcrcpu@gmail.com>
2023-05-08 12:49:35 -0700

    Adding new base database functionality

--- src/database.rs
+++ src/database.rs
@@ -9,6 +9,40 @@ use serde::Serialize;
 #[database("redis")]
 pub struct RedisDatabase(Pool);
 
+pub enum DatabaseOption {
+    GET,
+    SET,
+    RENAME,
+}
+
+impl std::fmt::Display for DatabaseOption {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", match self {
+            Self::GET => "GET",
+            Self::SET => "SET",
+            Self::RENAME => "RENAME",
+        })
+    }
+}
+
+pub async fn database<T: ToRedisArgs>(
+    option: DatabaseOption,
+    args: &T,
+    rds: &mut Connection<RedisDatabase>) -> Result<String, String> {
+    match redis::cmd(&format!("{}", option))
+        .arg(&args)
+        .query_async::<_, String>(&mut **rds)
+        .await
+    {
+        Ok(result) => Ok(result),
+        Err(error) => Err(format!(
+                "database.rs: database({}): Redis Cmd Failed to execute option query command; {}",
+                option,
+                error
+            )),
+    }
+}
+
 pub async fn database_get<T: ToRedisArgs>(
     args: &T,
     rds: &mut Connection<RedisDatabase>,
